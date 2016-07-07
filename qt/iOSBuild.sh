3891d4d5224582143aa58d45396a45d6c6f334a5
pasdam <paco@deepblue-nl.local>
2016-07-07 11:41:44 +0200

    Added quotes to support path with spaces

--- qt/iOSBuild.sh
+++ qt/iOSBuild.sh
@@ -11,6 +11,6 @@ PRO_FILE=
 
 # iOS build
 # TODO check if it already exists
-mkdir $OUT_DIR/iOS/
-$QT_DIR/$QT_VERSION/ios/bin/qmake $PRO_FILE -o $OUT_DIR/iOS/Makefile -r -spec macx-ios-clang CONFIG+=qtquickcompiler CONFIG+=production CONFIG+=release CONFIG+=iphoneos CONFIG+=device
+mkdir "$OUT_DIR/iOS/"
+$QT_DIR/$QT_VERSION/ios/bin/qmake "$PRO_FILE" -o "$OUT_DIR/iOS/Makefile" -r -spec macx-ios-clang CONFIG+=qtquickcompiler CONFIG+=production CONFIG+=release CONFIG+=iphoneos CONFIG+=device
 xcodebuild -project $OUTDIR/iOS/DateNow.xcodeproj
