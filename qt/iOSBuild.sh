74c19506dcdca4a05f75fc7d9401980a124f6478
pasdam <paco@deepblue-nl.local>
2016-07-07 16:10:15 +0200

    Added check if output directory already exists

--- qt/iOSBuild.sh
+++ qt/iOSBuild.sh
@@ -10,7 +10,8 @@ IN_DIR=
 PRO_FILE=
 
 # iOS build
-# TODO check if it already exists
-mkdir "$OUT_DIR/iOS/"
-$QT_DIR/$QT_VERSION/ios/bin/qmake "$PRO_FILE" -o "$OUT_DIR/iOS/Makefile" -r -spec macx-ios-clang CONFIG+=qtquickcompiler CONFIG+=production CONFIG+=release CONFIG+=iphoneos CONFIG+=device
-xcodebuild -project $OUTDIR/iOS/DateNow.xcodeproj
+if [ ! -d "$OUT_DIR" ]; then
+	mkdir "$OUT_DIR"
+fi
+$QT_DIR/$QT_VERSION/ios/bin/qmake "$PRO_FILE" -o "$OUT_DIR/Makefile" -r -spec macx-ios-clang CONFIG+=qtquickcompiler CONFIG+=production CONFIG+=release CONFIG+=iphoneos CONFIG+=device
+xcodebuild -project $OUTDIR/DateNow.xcodeproj
