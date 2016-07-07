4fe59f704da95b7fff47480d8cf4bf7249bc9934
pasdam <paco@deepblue-nl.local>
2016-07-07 11:33:09 +0200

    Added script to build iOS app with qt

---
+++ qt/iOSBuild.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# qt configuration
+QT_DIR=
+QT_VERSION=
+
+# project configuration
+OUT_DIR=
+IN_DIR=
+PRO_FILE=
+
+# iOS build
+# TODO check if it already exists
+mkdir $OUT_DIR/iOS/
+$QT_DIR/$QT_VERSION/ios/bin/qmake $PRO_FILE -o $OUT_DIR/iOS/Makefile -r -spec macx-ios-clang CONFIG+=qtquickcompiler CONFIG+=production CONFIG+=release CONFIG+=iphoneos CONFIG+=device
+xcodebuild -project $OUTDIR/iOS/DateNow.xcodeproj
