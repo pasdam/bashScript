3b409200404f149c1e32123f16f40102d3b93e8d
pasdam <paco@deepblue-nl.local>
2016-07-12 12:19:49 +0200

    Fixed mkdir command, in order to create also parent folders

--- qt/iOSBuild.sh
+++ qt/iOSBuild.sh
@@ -11,7 +11,7 @@ PRO_FILE=
 
 # iOS build
 if [ ! -d "$OUT_DIR" ]; then
-	mkdir "$OUT_DIR"
+	mkdir -p "$OUT_DIR"
 fi
 $QT_DIR/$QT_VERSION/ios/bin/qmake "$PRO_FILE" -o "$OUT_DIR/Makefile" -r -spec macx-ios-clang CONFIG+=qtquickcompiler CONFIG+=production CONFIG+=release CONFIG+=iphoneos CONFIG+=device
 xcodebuild -project $OUTDIR/DateNow.xcodeproj
